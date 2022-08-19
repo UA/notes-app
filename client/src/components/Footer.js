1f022b15a90c852ef33737aeb5cb4b96789d046c
ugur.ayaz <ugur.ayaz@metus.com>
2022-08-20 01:49:57 +0300

    footer updated

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,29 +2,26 @@ import React from 'react'
 
 function Footer() {
     return (
-        <div className="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center">
-            <a href="http://ugurayaz.com/" className="text-indigo-500">
-                ugurayaz.com
-            </a>
-            <p className="leading-normal my-5">
-                Developed by
+        <footer className="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center">
+             <p className="leading-normal my-5">
+                Made with by
                 <br />
                 Uğur Ayaz
             </p>
             <span className="inline-flex">
-                <a
-                    href="https://www.linkedin.com/in/ugurayaz23/"
-                    className="text-gray-500"
-                >
+                <a href="http://ugurayaz.com/" className="text-gray-500 ">
                     <svg
-                        fill="currentColor"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        className="w-5 h-5"
+                        className="h-5 w-5"
+                        fill="none"
                         viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        strokeWidth="2"
                     >
-                        <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9"
+                        />
                     </svg>
                 </a>
                 <a
@@ -57,8 +54,20 @@ function Footer() {
                         <path d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"></path>
                     </svg>
                 </a>
+                <a href="https://github.com/UA" className="ml-4 text-gray-500">
+                    <svg
+                        fill="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        viewBox="0 0 30 30"
+                        className="w-5 h-5"
+                    >
+                        <path d="M15,3C8.373,3,3,8.373,3,15c0,5.623,3.872,10.328,9.092,11.63C12.036,26.468,12,26.28,12,26.047v-2.051 c-0.487,0-1.303,0-1.508,0c-0.821,0-1.551-0.353-1.905-1.009c-0.393-0.729-0.461-1.844-1.435-2.526 c-0.289-0.227-0.069-0.486,0.264-0.451c0.615,0.174,1.125,0.596,1.605,1.222c0.478,0.627,0.703,0.769,1.596,0.769 c0.433,0,1.081-0.025,1.691-0.121c0.328-0.833,0.895-1.6,1.588-1.962c-3.996-0.411-5.903-2.399-5.903-5.098 c0-1.162,0.495-2.286,1.336-3.233C9.053,10.647,8.706,8.73,9.435,8c1.798,0,2.885,1.166,3.146,1.481C13.477,9.174,14.461,9,15.495,9 c1.036,0,2.024,0.174,2.922,0.483C18.675,9.17,19.763,8,21.565,8c0.732,0.731,0.381,2.656,0.102,3.594 c0.836,0.945,1.328,2.066,1.328,3.226c0,2.697-1.904,4.684-5.894,5.097C18.199,20.49,19,22.1,19,23.313v2.734 c0,0.104-0.023,0.179-0.035,0.268C23.641,24.676,27,20.236,27,15C27,8.373,21.627,3,15,3z" />
+                    </svg>
+                </a>
             </span>
-        </div>
+        </footer>
     )
 }
 
